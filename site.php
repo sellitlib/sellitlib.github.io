f8789ffe7c9fb2a1c39e3a256d66b2a019a10047
Karem Kaba <kabakarem@gmail.com>
2021-08-05 11:31:01 +0000

    files are updated

---
+++ site.php
@@ -0,0 +1,24 @@
+<?php
+   $name = $_POST['name'];
+   $visitor_email = $_POST['email'];
+   $message = $_POST['message'];
+
+
+ $email_from = 'sellitliberia@gmail.com';
+
+ $email_subect = "New form Sbmission";
+ 
+ $email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                          "User message: $message.\n".
+     
+     
+     $to = "sellitliberia@gmail.com";
+
+     $headers = "From: $email_from \r\n";
+
+     $headers .= "Reply-To: $visitor_email \r\n";
+
+     mail($to,$email_subject,$email_body,$headers);
+ 
+      header("Location: contact.html");
